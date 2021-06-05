a7b65b8d1cb2b97849e67935fd3d11f24bfc6f60
Sangeetha29A <85391551+Sangeetha29A@users.noreply.github.com>
2021-06-05 15:16:02 +0530

    Add files via upload

---
+++ Note.jsx
@@ -0,0 +1,39 @@
+import React from"react";
+
+function Note() {
+  return (
+    <div className="note">
+      <h1>Use of Javascript:</h1>
+      <p> 
+      Adding interactive behavior to web pages and
+      JavaScript allows users to interact with web pages,
+      Creating web and mobile apps,
+      Building web servers and developing server applications and 
+      Game development.
+          </p>
+        <h1>Advantages of Javascript:</h1>
+        <p>
+           Speed and
+           Client-side JavaScript is very fast because it can be run immediately within the client-side browser,
+           Simplicity,
+           JavaScript is relatively simple to learn and implement,
+           Popularity,
+           Interoperability and
+           Server Load.
+      </p>
+    <h1>Difference between javascript and java:</h1>
+      <p>
+       Key differences between Java and JavaScript,
+       Java is an OOP programming language while Java Script is an OOP scripting language. 
+       Java creates applications that run in a virtual machine or browser while JavaScript code is run on a browser only. 
+       Java code needs to be compiled while JavaScript code are all in text.
+       </p>
+
+
+    </div> 
+   
+   );
+}        
+
+
+export default Note;
